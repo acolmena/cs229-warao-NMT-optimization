01d9d732a379b8e698e647562f313c23f74dc8b3
acolmena <anacolme.18@gmai.com>
2025-11-13 22:23:00 -0800

    initial code for extending embedding matrix to understand what was happening

--- models/expanding_embedding_matrix.py
+++ models/expanding_embedding_matrix.py
@@ -5,6 +5,9 @@ This code is adapted from: https://www.cs.columbia.edu/~johnhew//vocab-expansion
 import transformers
 import torch
 import pandas as pd
+import random
+
+random.seed(42) # for reproducibility
 
 MULTILINGUAL_MODEL_PATH = "facebook/mbart-large-50"
 GPT_2_PATH = 'gpt2'
@@ -14,25 +17,28 @@ warao_vocab = pd.read_csv('../data_exploration/output/warao_vocab.csv')
 warao_vocab_lst = list(warao_vocab['warao_words'])
 print(f"Preview vocab: {warao_vocab_lst[:5]}")
 
+# randomly sample 200 words in the vocab
+num_wrds = 200
+rando_warao_words = random.sample(warao_vocab_lst, k=num_wrds)
+
 
 tok = transformers.MBart50TokenizerFast.from_pretrained(MULTILINGUAL_MODEL_PATH, src_lang="pt_XX", tgt_lang="es_XX")
 # tok = transformers.GPT2Tokenizer.from_pretrained(GPT_2_PATH)
 model = transformers.MBartForConditionalGeneration.from_pretrained(MULTILINGUAL_MODEL_PATH)
 # model = transformers.AutoModelForCausalLM.from_pretrained(GPT_2_PATH)
 
-# see tokens generated for a single warao word
+# 1) See tokens generated for a single warao word
 tokens = tok.convert_ids_to_tokens(tok('janoko')['input_ids'])  # jisabaya means 'house'
 print(tokens)
 
-warao_sent = 'Ka janoko sanukira'
+# can investigate the same thing for an entire sentence
+warao_sent = 'Ka janoko sanukira'       # Translates to: our house is small
 tokens2 = tok.convert_ids_to_tokens(tok(warao_sent)['input_ids'])
 print(tokens2)
 
 
 
-# check initial translation: new tokens aren't useful yet since new words' embeddings haven't been trained yet
-# generated_answer2 = tok.decode(model.generate(**tok(sent2, return_tensors='pt'), do_sample=True)[0]). # generate next best tokens
-# print(generated_answer2)
+# 2) Check initial translation: new tokens aren't useful yet since new words' embeddings haven't been trained yet
 # encode source Warao text
 encoded_wa = tok(warao_sent, return_tensors="pt")
 # Decode the translated tokens
@@ -45,7 +51,7 @@ print(f"First translation, no extension yet: {translation}")
 
 
 # add tokens
-tok.add_tokens(warao_vocab_lst)
+tok.add_tokens(rando_warao_words)
 model.resize_token_embeddings(len(tok))
 # compute the distribution from which we’ll sample our new embeddings
 tokens_added = len(warao_sent)
